50fdaba8c9bec4d3313748fa83283f597d8b0d76
Влад <deputatovich@bk.ru>
2020-04-19 01:49:52 +0500

    делал персов, осознал вселенную

--- St.py
+++ St.py
@@ -1,5 +1,4 @@
 import random
-from time import sleep
 from Hero import Strength
 from Hero import Intelligence
 
@@ -8,26 +7,37 @@ from Hero import Intelligence
 class Tank(Strength):
 
     def __init__(self, _st, _ag, _int):
-        super(Strengtha).__init__(_st, _ag, _int):
+        super(Strength).__init__(_st, _ag, _int):
         self.skills =[self.protection,self.madman]
 
     def protection(self):
-        self.health += enemy_hero.damage
-    # 100% защита от удара
+        pass
+    # доделаю завтра
 
-    def madman(self):
-        bool = random.uniform(0, 100)
-        self.health -= bool
-        self.damage += bool
+    def madman(self,enemy_hero):
+        if enemy_hero.health < self.health:
+        self.health -= random.uniform(0, 100)
+        else:
+        self.damage += random.uniform(0, 100)
         # сносит рандомно кол-во хп протиивнку и при этом наност себе слолько же
 
 
 class ShieldDearer(Intelligence):
 
      def __init__(self, _st, _ag, _int):
-         super(Strengtha).__init__(_st, _ag, _int):
-         self.skills = [self.potion, pass ]
+         super(Intelligence).__init__(_st, _ag, _int):
+         self.skills = [self.potion, self.tebe_hana ]
 
-     def potion(self, _int):
-         enemy_hero._int -= random.uniform(0, 10)
-     # Понижает шанс выпода скла у противнка
+     def potion(self, enemy_hero):
+         enemy_hero.int -= random.uniform(0, 10)
+         if enemy_hero.st < self.int and  random.uniform(0, 10) < 4:
+             self.health += 200
+        #если у противникаа _st больше чем твой _int то +ХП
+
+
+
+     def tebe_hana(self):
+
+    # если _st = 30 и выподает рандомм то противник умрает
+
+
