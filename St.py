5021183029c9ddb0096fda44de36e6483f255d9b
Влад <deputatovich@bk.ru>
2020-05-25 04:00:37 +0500

    Доделал персов

--- St.py
+++ St.py
@@ -8,11 +8,21 @@ class Tank(Strength):
         super().__init__(35, 10, 5)
         self.skills = [self.protection, self.madman]
 
-    def protection(self):
-        pass
-    # доделаю завтра
+    def protection(self, _int):
+        example = {"25 - 36 * 2": -47, "47 - 382": -335, "-59 + 29": -30,
+                   }
+        key, value = example.popitem()
+        print("Решите пример: ", key)
+        p_int = random.uniform(1, 11)
+        if int(input()) == value:
+            self.int +=p_int
+            print(f'Ответ верный, вы получаете {p_int} интелекта .')
+        else:
+            self.int -= p_int
+            print(f'Ответ не верный, вы теряете {p_int} интелекта .')
 
-    def madman(self,enemy_hero):
+
+    def madman(self, enemy_hero):
         damage = random.uniform(0, 100)
         if enemy_hero.health < self.health:
             self.health -= damage
@@ -33,6 +43,8 @@ class ShieldDearer(Intelligence):
             self.health += 200
         # если у противникаа _st больше чем твой _int то +ХП
 
-    def tebe_hana(self):
-        pass
+    def tebe_hana(self, _st, enemy_hero):
+        if (_st < 30) and (self.health > random.uniform(0, 100) ):
+            enemy_hero.health = 0
+
     # если _st = 30 и выподает рандомм то противник умрает
