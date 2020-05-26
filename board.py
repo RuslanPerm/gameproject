380ed0cb23ef3d02de1d8b27cb379b9a4cdc33f5
RuslanPerm <manfuckschool@mail.ru>
2020-05-26 17:12:57 +0500

    Большая перестройка

--- board.py
+++ board.py
@@ -1,5 +1,5 @@
 class Board:
-    def __init__(self, x_size, y_size):
+    def __init__(self, x_size=15, y_size=15):
         self.field = [[0] * y_size for _ in range(x_size)]
         self.x_size = x_size
         self.y_size = y_size
@@ -9,29 +9,20 @@ class Board:
         self.field = [[], []]
         self.status_move = 0
 
-    def fight(self, hero, enemy):
+    def fight(self, hero, enemy_hero):
         while True:
-            hero.stun_status -= 1 if hero.stun_status > 0 else hero.strike(enemy)
+            hero.degree_of_dying -= 1 if hero.degree_of_dying > 0 else hero.strike(enemy)
             if (hero.health < 0) and (enemy.health < 0):
-                condition = self.check_hero_health()
-                if condition == 'конец':
+                if self.check_hero_health() == 'конец':
                     return None
                 return False
             hero, enemy = enemy, hero
 
     def add(self, hero, x, y):
         self.hero_list.append(hero)
-        if x > (self.x_size - 1) or x < 0 or y > (self.y_size - 1) or y < 0:
-            print('емае, чё делаешь, выйди и зайди нормально')
-            return False
-        elif self.field[x][y] != 0:
-            print('тут занято')
-            return False
-        else:
-            self.field[x][y] = hero
-            hero.x = x
-            hero.y = y
-            return True
+        self.field[x][y] = hero
+        hero.x, hero.y = x, y
+        return True
 
     def team_add(self):
         for hero in self.hero_list:
@@ -44,17 +35,18 @@ class Board:
 
     def heroes_status(self):
         for hero in self.hero_list:
-            print(f"{hero.name} ({hero.team}): [{hero.x}, {hero.y}], {hero.health} HP", sep="\n")
+            print(f"{hero.name} из команды {hero.team}: в точке [{hero.x}, {hero.y}], "
+                  f"запас здоровья{hero.health} единиц", sep="\n")
 
     def check_team(self):
         for i in range(len(self.hero_list) - 1):
             for j in range(i + 1, len(self.hero_list)):
                 if (self.hero_list[i].x == self.hero_list[j].x) and (self.hero_list[i].y == self.hero_list[j].y):
                     if self.hero_list[i].team == self.hero_list[j].team:
-                        return 'ally'
+                        return 'занято'
                     condition = self.fight(self.hero_list[i], self.hero_list[j])
                     if condition is None:
-                        return 'break'
+                        return 'стоп'
                     return False
         return True
 
@@ -64,9 +56,8 @@ class Board:
                 print(f'{hero.name} ({hero.team}) was killed.\n')
                 self.hero_list.remove(hero)
                 self.team_dict[hero.team].remove(hero)
-                condition = self.game()
-                if condition is None:
-                    return 'end'
+                if self.game() is None:
+                    return 'конец'
 
     def move(self, hero):
         command = input().lower()
@@ -77,7 +68,7 @@ class Board:
                 else:
                     hero.x = hero.x - 1
                     step_status = self.check_team()
-                    if step_status == 'ally':
+                    if step_status == 'занято':
                         print('Тут уже союзник стоит.', end="\n")
                     else:
                         return step_status
@@ -87,7 +78,7 @@ class Board:
                 else:
                     hero.x = hero.x + 1
                     step_status = self.check_team()
-                    if step_status == 'ally':
+                    if step_status == 'занято':
                         print('Тут уже союзник стоит.', end="\n")
                     else:
                         return step_status
@@ -97,7 +88,7 @@ class Board:
                 else:
                     hero.y = hero.y + 1
                     step_status = self.check_team()
-                    if step_status == 'ally':
+                    if step_status == 'занято':
                         print('Тут уже союзник стоит.\n')
                     else:
                         return step_status
@@ -108,7 +99,7 @@ class Board:
                 else:
                     hero.y = hero.y - 1
                     step_status = self.check_team()
-                    if step_status == 'ally':
+                    if step_status == 'занято':
                         print('Тут уже союзник стоит.', end="\n")
                     else:
                         return step_status
@@ -116,32 +107,11 @@ class Board:
                 self.heroes_status()
             elif command == 'остановочка':
                 return False
-            elif command == 'закрыть':
-                return 'break'
+            elif command == 'камаз камаз я на целый час':
+                return 'стоп'
             else:
-                print('Используйте команды "вверх", "вниз", "влево", "вправо"')
-
-    # def game(self):
-    #     while True:
-    #         print(f"Ходит команда{self.status_move}")
-    #         x, y = map(int, input().split())
-    #         x_now, y_now = map(int, input().split())
-    #         self.move(x, y, x_now, y_now)
-    #         count_0 = 0
-    #         count_1 = 0
-    #         for i in range(self.x_size):
-    #             for j in range(self.y_size):
-    #                 if self.field[i][j] != 0:
-    #                     if self.field[i][j].team == 0:
-    #                         count_0 += 1
-    #                     else:
-    #                         count_1 += 1
-    #         if count_0 == 0:  # доработать со скилами и способностями
-    #             print('Team 1 win!')
-    #             break
-    #         elif count_1 == 0:
-    #             print('Team 0 win!')
-    #             break
+                print('Используйте команды "вверх", "вниз", "влево", "вправо", '
+                      '"камаз камаз я на целый час", "остановочка", "здоровье"')
 
     def game(self):
         self.team_add()
@@ -149,13 +119,8 @@ class Board:
             step_status = True
             for hero in self.hero_list:
                 step_status = True
-                print(f'\nTeam {hero.team} turn. \n{hero.name} has started moving.\n')
-                while (self.status_move < 3) and step_status:
-                    self.status_move += 1
-                    step_status = self.move(hero)
-                    if step_status is False:
-                        step_status = False
-                        break
+                print(f'{hero.name} выбирает куда ступить\n')
+                step_status = self.move(hero)
                 self.status_move = 0
                 if step_status is False:
                     break
